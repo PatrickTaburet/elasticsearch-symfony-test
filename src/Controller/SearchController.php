f26d509a6636d45831ac988de85d71ad67d33dc6
Patrick Taburet <taburet.patrick@gmail.com>
2025-03-26 19:17:16 +0100

    ElasticSearch implementation ok.

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,12 @@
 namespace App\Controller;
 
 use App\Form\Type\SearchFormType;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\MatchPhrase;
+use Elastica\Query\MatchQuery;
+use Elastica\Query\Range as QueryRange;
+use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +16,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
+    public function __construct(
+        private readonly PaginatorInterface $paginator,
+        private readonly PaginatedFinderInterface $finder
+    )
+    {
+        
+    }
     #[Route('/search', name: 'app_search')]
     public function index(Request $request): Response
     {
         $form = $this->createForm(SearchFormType::class);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()){
+            /** @var SearchModel $data */
+            $data = $form->getData();
+            $page = $request->query->getInt('page', 1);
+
+            $boolQuery = new BoolQuery();
+            if ($data->query){
+                $boolQuery->addMust(new MatchPhrase('title', $data->query));
+            }
+            if ($data->category){
+                $boolQuery->addFilter(new MatchQuery('category.id', $data->category->getId()));
+            }
+            if ($data->createdThisMonth){
+                $range = new QueryRange('createdAt', [
+                    'gte' =>(new \DateTimeImmutable('-1 month'))->format('Y-m-d'),
+                ]);
+                $boolQuery->addFilter($range);
+            }
+            $result = $this->finder->createPaginatorAdapter($boolQuery);
+            $pagination = $this->paginator->paginate($result, $page);
+        }
+
         return $this->render('search/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'pagination' => $pagination ?? []
         ]);
     }
 }
